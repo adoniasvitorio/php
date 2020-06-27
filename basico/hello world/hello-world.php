29b1366033a3f6f8ff72697156292da8e367fe08
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-06-27 15:44:14 -0300

    alterado nome do arquivo

--- basico/hello world/hello-world.php
+++
@@ -1,6 +0,0 @@
-<?php //instrução para indicar que, a partir deste ponto um codigo php sera executado.
-
-echo "hell world !"; // instrução para devolver uma informação em tela ou terminal. 
-
-//instrução para indicar o fim do codigo php.
-?>
