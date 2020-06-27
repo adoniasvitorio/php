43cdfd252950cd5b534775169fbedc693a4ac2dc
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-06-27 15:03:32 -0300

    criado meu hello world em php

---
+++ basico/hello world/hello-world.php
@@ -0,0 +1,6 @@
+<?php //instrução para indicar que, a partir deste ponto um codigo php sera executado.
+
+echo "hell world !"; // instrução para devolver uma informação em tela ou terminal. 
+
+//instrução para indicar o fim do codigo php.
+?>
