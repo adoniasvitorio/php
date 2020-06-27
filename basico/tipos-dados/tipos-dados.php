b3b3c43c92d45eef92be884aaea0e67f4a63a5df
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-06-27 15:37:41 -0300

    entendido sobre tipos de variveis

---
+++ basico/tipos-dados/tipos-dados.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+php e uma linguagem dinamica.
+Signigica que ela consegue identificar o tipo da variavel a partir de seu valor.
+Não existe a nescessidade de informar ao php o tipo de cada variavel, isto e opcional.
+*/
+
+$nome = "Adonias Vitorio";
+$idade = 26;
+$salario = 5550.75;
+$vedadeiro = true;
+$falso = false;
+
+/*
+Dica: 
+gettype($nomeDaVariavel) para recuperar o tipo da variavel.
+*/
+?>
