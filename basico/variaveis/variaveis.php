5ad13f3c12cc4ebaa2ad1f2974d8cbdcc03430d2
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-06-27 15:04:16 -0300

    entendido conceito de variaveis no php

---
+++ basico/variaveis/variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "Adonais Vitorio";
+echo $nome;
+
+$idade = 26;
+echo $idade;
+
+/*
+
+Para indicar uma variavel no php utiliza-se o sinal de [ $ ] seguido do nome desta variavel.
+se quisermos atribuir um valor a ela passamos so sinal de [ = ], seguido do valor que queremos atribuir. 
+
+*/
+?>
