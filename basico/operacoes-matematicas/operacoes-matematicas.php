a50aeebcf98da405a7d3f13dfd15bd07f7901b59
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-06-27 15:18:20 -0300

    entendido sobre operacoes matematicas

---
+++ basico/operacoes-matematicas/operacoes-matematicas.php
@@ -0,0 +1,22 @@
+<?php
+
+$numeroUm = 10;
+$numeroDois = 5;
+
+//exemplo de adição.
+$soma = $numeroUm + $numeroDois;
+echo $soma;
+
+//exemplo de subtração.
+$subtracao = $numeroUm - $numeroDois;
+echo $subtracao;
+
+//exemplo de multiplicação.
+$multiplicacao = $numeroUm * $numeroDois;
+echo $multiplicacao;
+
+//exemplo de divisão.
+$divisao = $numeroUm / $numeroDois;
+echo $divisao;
+
+?>
